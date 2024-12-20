eff69197f6f29d9c196b51edd138ff310da2d4f2
Ilia-Trayanovich <trojanovich.ilia@gmail.com>
2024-12-20 13:27:37 +0300

    index and about ready in the main app

--- main/views.py
+++ main/views.py
@@ -4,12 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - Home'
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('about')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о том, какой замечательный магазин и прекрасный товар'
+    }
+
+    return render(request, 'main/about.html', context)
    
