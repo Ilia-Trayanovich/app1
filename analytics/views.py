c435208a542fd8afc907311b8512ba6f260e0073
Ilia-Trayanovich <trojanovich.ilia@gmail.com>
2025-02-18 11:28:24 +0300

    add requerements.txt

--- analytics/views.py
+++ analytics/views.py
@@ -1,116 +1,31 @@
 from django.shortcuts import render
-from .utils import get_graph_data
-import matplotlib
-matplotlib.use('Agg')  
-import matplotlib.pyplot as plt
-import os
 
-# Путь для сохранения графиков
-GRAPH_DIR = '/static/analytics/graphs/'
-
-def create_graph(x, y, title, xlabel, ylabel, filename):
-    """Создает график и сохраняет его в файл"""
-    plt.figure(figsize=(10, 6))
-    plt.plot(x, y, marker='o')
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    file_path = os.path.join(GRAPH_DIR, filename)
-    plt.savefig(file_path, format='png')
-    plt.close()
-    return file_path
-
-def create_histogram(data, title, xlabel, ylabel, filename):
-    """Создает гистограмму и сохраняет её в файл"""
-    plt.figure(figsize=(10, 6))
-    plt.hist(data, bins=20, edgecolor='black')
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    file_path = os.path.join(GRAPH_DIR, filename)
-    plt.savefig(file_path, format='png')
-    plt.close()
-    return file_path
 
 def index(request):
-    graph_data = get_graph_data()
-
-    os.makedirs(GRAPH_DIR, exist_ok=True)
-    
-    # Генерация графиков
-    graphs = [
+    graphs = (
         {
+            "image": "avg_price_per_sqm.png",
             "title": "График изменения средней стоимости квадратного метра по дням",
-            "image": create_graph(
-                graph_data['dates'], graph_data['avg_prices_per_sqm'],
-                "Средняя цена за квадратный метр по дате", "Дата", "Цена за м²",
-                "avg_price_per_sqm.png"
-            )
         },
         {
+            "image": "avg_prices.png",
             "title": "График изменения средней стоимости квартир по дням",
-            "image": create_graph(
-                graph_data['dates'], graph_data['avg_prices'],
-                "Средняя стоимость квартир по дате", "Дата", "Цена квартиры",
-                "avg_prices.png"
-            )
         },
         {
+            "image": "avg_sizes.png",
             "title": "График изменения средней площади квартиры по дням",
-            "image": create_graph(
-                graph_data['dates'], graph_data['avg_sizes'],
-                "Средняя площадь квартиры по дате", "Дата", "Площадь (м²)",
-                "avg_sizes.png"
-            )
         },
         {
+            "image": "floor_distribution.png",
             "title": "График распределения этажей по квартирам",
-            "image": create_histogram(
-                graph_data['floors'], "Распределение этажей квартир",
-                "Этаж", "Частота", "floor_distribution.png"
-            )
         },
-        {
-            "title": "Распределение стоимости квартир",
-            "image": create_histogram(
-                graph_data['prices'], "Распределение стоимости квартир",
-                "Стоимость квартиры", "Частота", "price_distribution.png"
-            )
-        },
-        {
-            "title": "Распределение площади квартир",
-            "image": create_histogram(
-                graph_data['sizes'], "Распределение площади квартир",
-                "Площадь квартиры (м²)", "Частота", "size_distribution.png"
-            )
-        },
-        {
-            "title": "График зависимости цены от площади",
-            "image": create_graph(
-                graph_data['sizes'], graph_data['prices'],
-                "Зависимость цены от площади", "Площадь (м²)", "Цена квартиры",
-                "price_vs_size.png"
-            )
-        },
-        {
-            "title": "График зависимости цены от этажа",
-            "image": create_graph(
-                graph_data['floors'], graph_data['prices'],
-                "Зависимость цены от этажа", "Этаж", "Цена квартиры",
-                "price_vs_floor.png"
-            )
-        },
-        {
-            "title": "График числа квартир по дням",
-            "image": create_graph(
-                graph_data['dates'], graph_data['num_flats'],
-                "Число квартир по дням", "Дата", "Количество квартир",
-                "num_flats_per_day.png"
-            )
-        }
-    ]
+        {"image": "price_distribution.png", "title": "Распределение стоимости квартир"},
+        {"image": "price_vs_floor.png", "title": "График зависимости цены от этажа"},
+        {"image": "num_flats_per_day.png", "title": "График числа квартир по дням"},
+    )
 
-    return render(request, 'analytics/index.html', {
-        'content': 'Аналитика данных о квартирах',
-        'graphs': graphs
-    })
+    return render(
+        request,
+        "analytics/index.html",
+        {"content": "Аналитика данных о квартирах", "graphs": graphs},
+    )
--- parser/tasks.py
+++ parser/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 import logging
 from .parser import run_parsing
+from analytics.utils import generate_graphs
 
 logger = logging.getLogger(__name__)
 
@@ -17,3 +18,13 @@ def run_parser_task():
 @shared_task
 def test_task():
     return 'Задача выполнена успешно!'
+
+
+@shared_task
+def generate_graphs_task():
+    logger.info('Графики создаются.')
+    try:
+        generate_graphs()
+        logger.info('Графики успешно созданы.')
+    except Exception as e:
+        logger.error(f"Произошла ошибка: {e}")
